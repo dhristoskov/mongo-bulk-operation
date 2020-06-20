c3500329d364a06b89d02ce34e03fa7f87ff9a3c
Dimitar Hristoskov <10851930+dhristoskov@users.noreply.github.com>
2020-06-20 18:29:53 +0200

    commit

---
+++ controllers/students-controller.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const fs = require('fs');
+
+const Student = require('../models/student');
+
+const addData = async ( req, res ) => {
+    let file;
+    let fileName;
+    let studentsCounter = 0;
+
+    const studentsArr = []
+     
+    if(req.files){      
+        file = req.files.file;
+        fileName = file.name;
+        await file.mv('./resources/'+ fileName, (err) => {
+            if(err){
+                res.send(err)
+            }
+        });
+    }
+
+    fs.readFile(`./resources/${fileName}`, (err, data) => {
+        if(err) {
+            throw err;
+        }
+        const array = data.toString().split("\n").map(item => item.trim());
+        for(i in array) {
+            const line = array[i].split(',').map(e => e.trim());
+            const [ fname, lname, age, grade ] = line;
+
+            const student = new Student ({
+                firstName: fname,
+                lastName: lname,
+                age: age,
+                grade: grade,
+                result: ''
+            });    
+            studentsArr.push(student);
+        }
+        
+        try{
+            const bulk = Student.collection.initializeOrderedBulkOp();
+            studentsCounter = studentsArr.length;
+
+            studentsArr.forEach((el) => {
+                bulk.insert(el);
+                studentsCounter--
+
+                // if(studentsCounter % 1000 === 0){
+                //     bulk.execute()
+                //     bulk = Student.collection.initializeOrderedBulkOp();
+                // }
+                if(studentsCounter === 0){
+                    bulk.execute();
+                }
+            });
+            res.json({ students: studentsArr.map(student => student.toObject({ getters: true })) });
+        }catch(err){
+            console.error(err.message);
+            res.status(500).send('Server Error!');
+        }
+    });
+}
+
+const updateData = async ( req, res ) => {
+    const { minGrade, firstName } = req.body; 
+    try{
+        const bulk = Student.collection.initializeOrderedBulkOp();
+        bulk.find({grade: {$lte: minGrade}}).upsert().update({$set: {result: 'Poor'}})
+        bulk.find({grade: {$gt: minGrade}}).upsert().update({$set: {result: 'Excellent'}})
+        bulk.find({firstName: firstName}).remove()
+        bulk.execute();
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error!');
+    }
+    res.status(200).json('Data updated')
+}
+
+exports.addData = addData;
+exports.updateData = updateData;
